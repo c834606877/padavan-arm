4db70a27d872fe4aa3c437c3ff27e08e376372b9
c834606877 <c834606877@163.com>
2026-07-22 22:29:07 +0800

    Remove unnecessary ampersands from install commands

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,9 +4,9 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor > /usr/share/k
 
 apt update
 
-apt install -y libtool-bin  autopoint gettext &
+apt install -y libtool-bin  autopoint gettext
 
-apt install -y groff gperf autoconf-archive yodl gtk-doc-tools  python3-docutils python3-setuptools python3-dev libusb-dev libxml-parser-perl &
+apt install -y groff gperf autoconf-archive yodl gtk-doc-tools  python3-docutils python3-setuptools python3-dev libusb-dev libxml-parser-perl
 
 apt install -y fakeroot flex bc help2man libltdl-dev texinfo kmod 
 
